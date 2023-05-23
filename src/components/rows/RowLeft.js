59fc2c1bddf4b52ab64cfd27eed4e88e02f35c18
D. McDonald <d.mcdonald@MACs-MacBook-Pro.local>
2023-05-23 15:25:07 -0500

    update

--- src/components/rows/RowLeft.js
+++ src/components/rows/RowLeft.js
@@ -6,13 +6,13 @@ export default function RowLeft(props) {
   return (
     <Container fluid id="about">
         <Row fluid className='section-row'>
-            <Col xs={12} md={8} align='left'>
+            <Col xs={12} md={6} align='left'>
               <Container className='row-text-container row-text-left-container'>
                 <h2 className='section-heading'>{props.sectionHeading}</h2>
                 <p>{props.sectionText}</p>
               </Container>
             </Col>
-            <Col xs={12} md={4} className="row-left-image-container"></Col>
+            <Col xs={12} md={6} className="row-left-image-container" order={{ sm: 1 }}></Col>
         </Row>
     </Container>
   )
--- src/components/rows/RowRight.js
+++ src/components/rows/RowRight.js
@@ -6,8 +6,8 @@ export default function RowRight(props) {
   return (
     <Container fluid>
         <Row fluid className='section-row'>
-            <Col xs={12} md={4} className="row-right-image-container"></Col>
-            <Col xs={12} md={8} align='right'>
+            <Col xs={12} md={6} className="row-right-image-container"></Col>
+            <Col xs={12} md={6} align='right'>
                 <Container className='row-text-container row-text-right-container'>
                 <h2 className='section-heading'>{props.sectionHeading}</h2>
                 <p>{props.sectionText}</p>
